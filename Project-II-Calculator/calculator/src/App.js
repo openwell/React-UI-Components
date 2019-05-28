cd0eeb33cfc876810131344c0e5e63ee142e039f
openwell <timileyinojo@yahoo.com>
2019-05-28 16:21:38 +0100

    added bug fix hooks to class

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,55 +1,64 @@
-import React, { useState } from "react";
+import React, { Component }  from "react";
 import "./App.css";
 import CalculatorDisplay from "./components/DisplayComponents/CalculatorDisplay";
 import NumberButton from "./components/ButtonComponents/NumberButton";
 import ActionButton from "./components/ButtonComponents/ActionButton";
-const App = () => {
-  const [myState, updateMyState] = useState({
-    screen: { output: 1 }
-  });
-  console.log(myState, updateMyState);
-  const style = {
+class App extends Component {
+  state={
+    screen: 1
+  };
+  // console.log(myState, updateMyState);
+  style = {
     flex2: { flex: "25%" },
-    flex2Red: { flex: "25%", background: "#A0001E" },
+    flex2Red: { flex: "25%", background: "#A0001E", color: 'white' },
     flex3: { flex: "75%" },
   };
 
-  const clickHandler = event => {
-    console.log(event.target.innerText);
+  clickHandler = event => {
+    let toUpdate = {...this.state}
+    toUpdate.screen = event.target.innerText;
+    this.setState = ({
+      screen: toUpdate
+    });
+    console.log(event.target.innerText, toUpdate);
   };
+  render() {
   return (
     <div className="container">
       <div className="layerOne">
-        <CalculatorDisplay output={myState.screen.output} />
+        <CalculatorDisplay output={this.state.screen} />
       </div>
       <div className="layerTwo">
         <ActionButton
-          click={clickHandler}
-          style={style.flex3}
+          click={this.clickHandler}
+          style={this.style.flex3}
           symbol={"Clear"}
         />
-        <ActionButton click={clickHandler} style={style.flex2Red} symbol={"÷"} />
+        <ActionButton click={this.clickHandler} style={this.style.flex2Red} symbol={"÷"} />
       </div>
       <div className="layerThree">
-        <NumberButton click={clickHandler} style={style.flex2} number={"7"} />
-        <NumberButton click={clickHandler} style={style.flex2} number={"8"} />
-        <NumberButton click={clickHandler} style={style.flex2} number={"9"} />
-        <ActionButton click={clickHandler} style={style.flex2Red} symbol={"x"} />
-        <NumberButton click={clickHandler} style={style.flex2} number={"4"} />
-        <NumberButton click={clickHandler} style={style.flex2} number={"5"} />
-        <NumberButton click={clickHandler} style={style.flex2} number={"6"} />
-        <ActionButton click={clickHandler} style={style.flex2Red} symbol={"-"} />
-        <NumberButton click={clickHandler} style={style.flex2} number={"1"} />
-        <NumberButton click={clickHandler} style={style.flex2} number={"2"} />
-        <NumberButton click={clickHandler} style={style.flex2} number={"3"} />
-        <ActionButton click={clickHandler} style={style.flex2Red} symbol={"+"} />
+        <NumberButton click={this.clickHandler} style={this.style.flex2} number={"7"} />
+        <NumberButton click={this.clickHandler} style={this.style.flex2} number={"8"} />
+        <NumberButton click={this.clickHandler} style={this.style.flex2} number={"9"} />
+        <ActionButton click={this.clickHandler} style={this.style.flex2Red} symbol={"x"} />
+        <NumberButton click={this.clickHandler} style={this.style.flex2} number={"4"} />
+        <NumberButton click={this.clickHandler} style={this.style.flex2} number={"5"} />
+        <NumberButton click={this.clickHandler} style={this.style.flex2} number={"6"} />
+        <ActionButton click={this.clickHandler} style={this.style.flex2Red} symbol={"-"} />
+        <NumberButton click={this.clickHandler} style={this.style.flex2} number={"1"} />
+        <NumberButton click={this.clickHandler} style={this.style.flex2} number={"2"} />
+        <NumberButton click={this.clickHandler} style={this.style.flex2} number={"3"} />
+        <ActionButton click={this.clickHandler} style={this.style.flex2Red} symbol={"+"} />
       </div>
       <div className="layerFour">
-        <NumberButton click={clickHandler} style={style.flex3} number={"0"} />
-        <ActionButton click={clickHandler} style={style.flex2Red} symbol={"="} />
+        <NumberButton click={this.clickHandler} style={this.style.flex3} number={"0"} />
+        <ActionButton click={this.clickHandler} style={this.style.flex2Red} symbol={"="} />
       </div>
     </div>
   );
+  }
 };
 
 export default App;
+
+
